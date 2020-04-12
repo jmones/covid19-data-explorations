fa9510e4fda072c1e7d5322232f408b8f2a32c1c
jmones <jmones@gmail.com>
2020-04-12 23:17:30 +0200

    Added pollution database (#16)

--- data-raw/prepare_all.R
+++ data-raw/prepare_all.R
@@ -3,10 +3,12 @@ library(here)
 
 source(here("data-raw", "catalan_municipalities.R"))
 source(here("data-raw", "./covid_tests_catalonia.R"))
+source(here("data-raw", "./pollution_catalonia.R"))
 source(here("data-raw", "./xema_stats_per_day.R"))
 
 prepare_all_plan <- bind_plans(
   catalan_municipalities_plan,
   covid_tests_catalonia_plan,
+  pollution_catalonia_plan,
   xema_stats_per_day_plan
 )
